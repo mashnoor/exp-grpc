cd94b23ed4a0c98533b7564e4ccad35b086a7708
Nowfel Mashnoor <nowfel.mashnoor@upaybd.com>
2022-02-10 17:29:58 +0600

    updated health probe firebasepush

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"exp-grpc/chat"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
 
 func main() {
-	fmt.Println("Hello")
+
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	chat.RegisterChatServiceServer(grpcServer, &chat.UnimplementedChatServiceServer{})
+
+	log.Printf("GRPC server listening on %v", lis.Addr())
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
